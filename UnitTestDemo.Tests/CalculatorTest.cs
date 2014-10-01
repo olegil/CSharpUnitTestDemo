c5a68923c695c01f24c3e7e39e378d271ba28018
Tomson Xu <tomson.tao.xu@gmail.com>
2014-10-01 18:47:57 +0800

    init version

---
+++ UnitTestDemo.Tests/CalculatorTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestDemo.Tests
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void Add_Input_First_1_Second_2_Return_3() {
+            //arrange
+            ICalculator target = new Calculator();
+            int firstNumber = 1;
+            int secondNumber = 2;
+            int expected = 3;
+
+            //act
+            int actual;
+            actual = target.Add(firstNumber, secondNumber);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ UnitTestDemo/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestDemo
+{
+    public class Calculator: ICalculator
+    {
+        public int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
